a40b24415f3eba11d81c4d81d26d3c7178515693
Zusakhe-Nyembezi <St10388340@vcconnect.edu.za>
2025-03-11 10:37:26 +0200

    Add new class

--- prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
+++ prjClasses/src/main/java/com/mycompany/prjclasses/PrjClasses.java
@@ -11,6 +11,12 @@ package com.mycompany.prjclasses;
 public class PrjClasses {
 
     public static void main(String[] args) {
+        
+        newClass nc = new newClass();
+        
+        nc.helloClass(); //have to use variable name 'nc' to communicate with the other class
+        
+        System.out.println(nc.strSecondClassVar);
         System.out.println("Hello World!, I just did my first github activity and hate reece now. :)");
     }
 }
